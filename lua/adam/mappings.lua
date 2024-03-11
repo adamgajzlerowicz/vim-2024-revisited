ad004e4d09d7725c2b91b241b42f8d1f23f8ea4b
Adam Gajzlerowicz <nelf86@gmail.com>
2024-03-11 11:09:16 +0100

    master

--- lua/adam/mappings.lua
+++ lua/adam/mappings.lua
@@ -111,6 +111,7 @@ vim.api.nvim_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", 
 vim.api.nvim_set_keymap("n", "gt", "<cmd>lua vim.lsp.buf.type_definition()<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "[g", "<cmd>lua vim.diagnostic.goto_prev()<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "]g", "<cmd>lua vim.diagnostic.goto_next()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader><cr>", "<cmd>lua vim.lsp.buf.code_action()<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", { noremap = true, silent = true })
 vim.keymap.set('n', '<Leader>d', vim.diagnostic.open_float, {noremap = true, silent = true, desc = 'LSP Diagnostics'})
 vim.keymap.set('n', '<Leader>q', vim.diagnostic.setqflist, {noremap = true, silent = true, desc = 'LSP Diagnostics Quickfix'})
