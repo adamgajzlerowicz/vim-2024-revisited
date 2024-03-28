16b4b206143e60ff5cd23c6425ee044e0dbb0749
Adam Gajzlerowicz <nelf86@gmail.com>
2024-03-28 21:58:48 +0100

    master

--- lua/adam/packages.lua
+++ lua/adam/packages.lua
@@ -261,29 +261,12 @@ require("lazy").setup({
 	},
 
 	{
-		"rcarriga/nvim-dap-ui",
-		config = function()
-			require("dapui").setup()
-			local dap, dapui = require("dap"), require("dapui")
-			dap.listeners.before.attach.dapui_config = function()
-				dapui.open()
-			end
-			dap.listeners.before.launch.dapui_config = function()
-				dapui.open()
-			end
-			dap.listeners.before.event_terminated.dapui_config = function()
-				dapui.close()
-			end
-			dap.listeners.before.event_exited.dapui_config = function()
-				dapui.close()
-			end
-		end,
-	},
-
-	{
 		"windwp/nvim-ts-autotag",
 		setup = function()
 			require("nvim-ts-autotag").setup()
 		end,
+	},
+	{
+		"onsails/lspkind.nvim",
 	},
 })
