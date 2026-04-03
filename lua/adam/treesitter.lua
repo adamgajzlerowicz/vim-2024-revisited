8c217c9e4bc0b10f628bb3ccdeeb22ed0fbec4ee
Adam Gajzlerowicz <nelf86@gmail.com>
2026-04-03 09:02:01 +0200

    push

--- lua/adam/treesitter.lua
+++ lua/adam/treesitter.lua
@@ -1,5 +1,5 @@
 require('nvim-treesitter.configs').setup({
-  ensure_installed = {'astro', 'tsx', 'typescript', 'html'},
+  ensure_installed = {'astro', 'go', 'tsx', 'typescript', 'html'},
   auto_install = true,
   highlight = {
     enable = true
